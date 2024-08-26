70d379f4c04a1c0ea95ec3f9087a1ff1f15c4ad0
frekerzzzz <kannamugesh135@gmail.com>
2024-08-26 18:37:11 +0530

    navbar commit

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -11,36 +11,51 @@ const Sidebar = () => {
     Logout();
   };
   return (
-    <div className="mains w-25 position-fixed sidebar-bg vh-100 d-flex flex-column align-items-center">
-      <div className="content-div w-100 text-center">
-        <h5 className=" fw-bolder mt-2">Student Registration System</h5>
-        <div className="imagediv d-flex justify-content-center mt-5">
-          <img src={Logo} alt="Logo image" height="250px" width="250px" />
-        </div>
-        <div className="button-div d-flex flex-column mt-5 w-100">
-          <Link to={"/dashboard"}>
-            <button className="btn bg-btn btn-outline-light my-2 mx-auto w-75">
-              Dashboard
-            </button>
-          </Link>
-
-          <Link to={"/add-students"}>
-            <button className="btn bg-btn btn-outline-light my-2 mx-auto w-75">
-              Add Students
-            </button>
-          </Link>
-        </div>
-        <div
-          className="logout-div d-flex gap-2 align-items-center justify-content-center mt-5"
-          onClick={handleLogout}
-        >
-          <img src={Log} alt="logout icon" />
-          <h5 className="text-white ">
-            <b>Sign out</b>
-          </h5>
+    <>
+      <div className="mobile-nav sidebar-bg w-100">
+        <div className=" d-flex">
+          <div className="image-div">
+            <img src={Logo} alt="Logo image" height="80px" width="80px" />
+          </div>
+          <div className=" d-flex justify-content-center align-items-center">
+            <ul className="d-flex gap-5">
+              <li>All Students</li>
+              <li>Add Students</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="mains w-25 position-fixed sidebar-bg vh-100 d-flex flex-column align-items-center">
+        <div className="content-div w-100 text-center">
+          <h5 className=" fw-bolder mt-2">Student Registration System</h5>
+          <div className="imagediv d-flex justify-content-center mt-5">
+            <img src={Logo} alt="Logo image" height="250px" width="250px" />
+          </div>
+          <div className="button-div d-flex flex-column mt-5 w-100">
+            <Link to={"/dashboard"}>
+              <button className="btn bg-btn btn-outline-light my-2 mx-auto w-75">
+                Dashboard
+              </button>
+            </Link>
+
+            <Link to={"/add-students"}>
+              <button className="btn bg-btn btn-outline-light my-2 mx-auto w-75">
+                Add Students
+              </button>
+            </Link>
+          </div>
+          <div
+            className="logout-div d-flex gap-2 align-items-center justify-content-center mt-5"
+            onClick={handleLogout}
+          >
+            <img src={Log} alt="logout icon" />
+            <h5 className="text-white ">
+              <b>Sign out</b>
+            </h5>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
